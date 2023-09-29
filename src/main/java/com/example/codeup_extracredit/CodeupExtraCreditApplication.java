e843106e43d8619d06d90295ed9b9ec22d5322fc
Presley Black <presleyblack24@gmail.com>
2023-09-29 11:30:50 -0500

    first commit

---
+++ src/main/java/com/example/codeup_extracredit/CodeupExtraCreditApplication.java
@@ -0,0 +1,13 @@
+package com.example.codeup_extracredit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeupExtraCreditApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeupExtraCreditApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/codeup_extracredit/CodeupExtraCreditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codeup_extracredit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeupExtraCreditApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
